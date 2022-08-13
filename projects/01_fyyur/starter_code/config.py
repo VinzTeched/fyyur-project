b640f7c86309145aff092675410b846299737ae6
Chibundo Ilonze <chibundoilonze@gmail.com>
2022-08-13 15:36:29 +0100

    Update

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -7,7 +7,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
+SECRET_KEY = "fyyur"
 
+# Disable FSADeprecationWarning: SQLALCHEMY_TRACK_MODIFICATIONS
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+# Connect to the database
+database_name = "fyyur"
+database_path = "postgresql://{}:{}@{}/{}".format(
+    "postgres", "postgres", "localhost:5432", database_name
+)
+
+SQLALCHEMY_DATABASE_URI = database_path
